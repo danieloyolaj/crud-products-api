527620b5f582ea854ba6592d1f3d2881f4f276e2
softerdo <danieloyolaj@gmail.com>
2022-10-11 20:34:16 -0400

    Configigure initModels Authentication sync with db

--- src/app.js
+++ src/app.js
@@ -1,27 +1,38 @@
 //Dependencies
 const express = require('express')
-
-//Files
-const { port } = require('./config')
+const db = require('./utils/database')
+const initModels = require('./models/initModels')
+const config = require('./config')
+const productRouter = require('./products/products.router')
 
 //Initial configs
 const app = express()
 
+//Authenticates if the credentials for the database are correct
+db.authenticate()
+    .then(() => {console.log('Successfully authenticated!')})
+    .catch((err) => console.log(err))
+
+//Synchronizes the models (tables) with the database
+db.sync()
+    .then(() => console.log('Database synced!'))
+    .catch(err => console.log(err))
+
 //Receiving data in json format
 app.use(express.json())
 
-//Using the routes files
-app.use('/', productRouter)
+//Using the routes files. Also using a prefix for the route
+app.use('/products', productRouter)
 
 //Default petition
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Server Ok!',
-        products: `localhost:${port}/api/v1/products/`
+        products: `localhost:${config.port}/api/v1/products/`
     })
 })
 
 //Listen to port
-app.listen(port, () => {
-    console.log(`Server started at port ${port}`)
+app.listen(config.port, () => {
+    console.log(`Server started at port ${config.port}`)
 })
---
+++ src/models/initModels.js
@@ -0,0 +1,8 @@
+const Products = require('./products.model')
+
+//This contains all the relationships
+const initModels = () => {
+    Products
+}
+
+module.exports = initModels
